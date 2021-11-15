44d3aceba27045945110843b971bc4e8cdd12989
charlottedaumal <92551313+charlottedaumal@users.noreply.github.com>
2021-11-15 15:17:54 +0100

    Unit tests week6

--- unit_tests_week6.py
+++ unit_tests_week6.py
@@ -1,13 +1,16 @@
-from functions_week6 import *
+from storkey_rule_week6 import *
 import numpy as np
 
-random_patterns = generate_patterns(80, 100)
-perturbed_pattern = perturb_pattern(random_patterns[4], 80)
-weights = storkey_weights(random_patterns)
-updating_state = dynamics(perturbed_pattern, weights, 20)
+index_perturbed = 4
+random_patterns = generate_patterns(80, 1000)
+perturbed_pattern = perturb_pattern(random_patterns[index_perturbed], 80)
+
+""" Uncomment the part of the test you want to run"""
+#weights = hebbian_weights(random_patterns)
+#weights = storkey_weights(random_patterns)
+#updating_state = dynamics(perturbed_pattern, weights, 20)
+#updating_state = dynamics_async(perturbed_pattern, weights, 20000, 3000)
 
 print()
-if np.allclose(updating_state[len(updating_state)-1], random_patterns[4]):
+if pattern_match(random_patterns, perturbed_pattern) == index_perturbed:
     print("Pattern recognised")
-else:
-    print("Miskin")
