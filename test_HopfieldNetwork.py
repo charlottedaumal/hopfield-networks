cde32587db16fd82017caceaa39ffbfeecdf882e
ClemenceKiehl <92544192+ClemenceKiehl@users.noreply.github.com>
2021-11-29 15:15:32 +0100

    Update test_HopfieldNetwork.py

--- test_HopfieldNetwork.py
+++ test_HopfieldNetwork.py
@@ -1,15 +1,19 @@
 import numpy as np
 from functions import *
 
-
 def test_generate_patterns():
     '''testing the values of the  generated patterns '''
-    assert((generate_pattern(5, 10)).all() == [-1, 1])
+    assert((generate_patterns(5, 10)).all() == [-1, 1])
+
+    '''testing the size of the generated patterns'''
+    assert((generate_patterns(5, 10)).shape[0] == 5)
+    assert((generate_patterns(5, 10)).shape[1] == 10)
+
 
 def test_perturb_pattern():
-    '''testing the values of the generated patterns'''
-    assert((perturb_pattern(3,7)).all() == [-1, 1])
-    
+    '''testing the values of the perturbed patterns'''
+    assert((perturb_pattern(3, 7)).all() == [-1, 1])
+
 
 def test_update():
     assert(update(np.array([[2, 5, 6, 7], [4, 5, 6, 9]]), np.array([[1, 1], [1, 1]])) - np.array([[1, 1, 1, 1],
@@ -35,4 +39,3 @@ def test_storkey_weights():
     '''testing the size of the matrix'''
     weight_matrix = storkey_weights(np.array([[1, 1, -1, -1], [1, 1, -1, 1], [-1, 1, -1, 1]]))
     assert(weight_matrix.shape[0] == weight_matrix.shape[1])
-
