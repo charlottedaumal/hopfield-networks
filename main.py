47a93641ecc1b8368e0ea0b01fd7ac17a1473534
charlottedaumal <92551313+charlottedaumal@users.noreply.github.com>
2021-11-29 19:31:16 +0100

    New design of main

--- main.py
+++ main.py
@@ -1,147 +1,71 @@
-# Import of packages/useful files
 from functions import *
 
-# ---------------------------------------------------------------------------------------------------------------------------------
-
-# Code of week5
-
-"""Determination of global parameters"""
-N = 50
-M = 3
-base = [-1, 1]
-
-"""Creation of M random binary patterns"""
-patterns = np.empty(M*N).reshape(M, N)
-for i in range(0, M):
-    patterns[i] = np.array(rd.choices(base, k=N))
-
-"""Creation of the weight matrix with the Hebbian rule"""
-W = np.zeros(N * N).reshape(N, N)
-for i in range(0, N):
-    for j in range(0, N):
-        if i != j:
-            patterns_sum = 0
-            for z in range(0, M):
-                patterns_sum += patterns[z][i] * patterns[z][j]
-                W[i][j] = (1 / M) * patterns_sum
-
-"""Global verification on weight matrix
-print(W)
-   Verification symmetric matrix
-W_transpose = np.transpose(W)
-if W.all() == W_transpose.all():
-   print("symmetric matrix")"""
-
-"""Generation of pattern p0 by changing 3 random values of one of the memorized patterns"""
-p0 = patterns[0]
-counter = 0
-while counter < 3:
-    index = rd.choices(np.linspace(0, N-1, N, dtype=int))
-    p0[index] = rd.choices(np.linspace(-N, N, N**2), k=1)
-    counter += 1
-
-"""Applying the update rule"""
-T = 20
-p_t = p0
-for t in range(0, T):
-    p_t1 = np.sign(np.dot(W, p_t))
-    if np.allclose(p_t1, p_t):
-        print()
-        print("Results of unit tests of week 5 :")
-        print(f"Convergence reached after {t} iteration(s)")
-        break
-    p_t = p_t1
-    
-# ---------------------------------------------------------------------------------------------------------------------------------
-
-# Unit tests of week6
-
-index_perturbed = 4
-random_patterns = generate_patterns(80, 1000)
-perturbed_pattern = perturb_pattern(random_patterns[index_perturbed], 80)
-
-""" Uncomment the part of the test you want to run"""
-#weights = hebbian_weights(random_patterns)
-#weights = storkey_weights(random_patterns)
-#updating_state = dynamics(perturbed_pattern, weights, 20)
-#updating_state = dynamics_async(perturbed_pattern, weights, 20000, 3000)
-
-"""Verifies if the network retrieve the original pattern"""
-if pattern_match(random_patterns, perturbed_pattern) == index_perturbed:
-    print()
-    print("Results of unit tests of week 6 :")
-    print("Pattern recognised")
-    
-# ---------------------------------------------------------------------------------------------------------------------------------
-
-# Unit tests of week7
-
-# Energy plotting tests
-
-"""creates random patterns, perturbs one of them and stores them in a Hopfield network"""
-index_perturbed = 4
-random_patterns = generate_patterns(50, 100)
-perturbed_pattern = perturb_pattern(random_patterns[index_perturbed], 10)
-random_patterns[index_perturbed] = perturbed_pattern
-hopfield_network = random_patterns
-
-
-"""using the hebbian rule / with synchronous update rule or asynchronous update rule"""
-weights_h = hebbian_weights(hopfield_network)
-state_history_h = dynamics(perturbed_pattern, weights_h, 20)
-state_history_h_a = dynamics_async(perturbed_pattern, weights_h, 3000, 1000)
-
-
-"""using the storkey rule / with synchronous update rule or asynchronous update rule"""
-weights_s = storkey_weights(hopfield_network)
-state_history_s = dynamics(perturbed_pattern, weights_s, 20)
-state_history_s_a = dynamics_async(perturbed_pattern, weights_s, 3000, 1000)
-
-
-"""evaluating and comparing the energy of each of the states"""
-"""trying with state_history_s"""
-
-
-energy_list_s_a = energy(state_history_s_a[1], weights_s)
-energy_list_s = energy(state_history_s[1], weights_s)
-energy_list_h = energy(state_history_h[1], weights_h)
-energy_list_h_a = energy(state_history_h_a[1], weights_h)
-
-plt.figure(figsize=(5, 7))
-plt.plot(np.arange(0, len(energy_list_s), step=1), energy_list_s, 'b')
-plt.ylabel("energy")
-plt.xlabel("time[s]")
-plt.title("energy-time plot")
-plt.show()
-
-# Visualization tests
-
-"""Initializing the checkerboard, the randome patterns and the perturbed pattern"""
-checkerboard = create_checkerboard(50)
+# Generates the matrix of random patterns
 random_patterns = generate_patterns(50, 2500)
+
+# Creation of the checkerboard and insertion at the end of the patterns' matrix
+checkerboard = create_checkerboard(50)
 random_patterns[-1] = checkerboard.flatten()
+
+# Choose randomly an index in the matrix of patterns and perturb the associated pattern
 perturbed_pattern = perturb_pattern(random_patterns[-1], 1000)
 
-""" Uncomment the part of the test you want to run"""
+# Computation of the two weights matrices using the Hebbian and the Storkey learning rules
+# depending on what the user chooses through a keyboard interaction
+weights = np.zeros(random_patterns.shape[1])
+value = input("Please enter 'h' if you want to dot all the computations with the hebbian weights matrix "
+              "or 's' if you want to do it with the storkey weights, "
+              "then press 'Enter' :\n")
+if value == 'h':
+    weights = hebbian_weights(random_patterns)
+elif value == 's':
+    weights = storkey_weights(random_patterns)
+else:
+    print("ERROR : You didn't enter a proper character")
 
-"""Calculating the weight matrix according to Hebbian rule or Storkey rule"""
-weights_heb = hebbian_weights(random_patterns)
-# weights_sto = storkey_weights(random_patterns)
-updating_state_dynamics = dynamics(perturbed_pattern, weights_heb, 20)
-# updating_state_dynamics_async = dynamics_async(perturbed_pattern, weights_sto, 20000, 3000)
+# Computation of the state history using the dynamics and the asynchronous dynamics function
+state_history_sync = dynamics(perturbed_pattern, weights, 30000)
+state_history_async = dynamics_async(perturbed_pattern, weights, 30000, 10000)
 
-reshaped_updating_state_dynamics = []
-for i in range(len(updating_state_dynamics)):
-    reshaped_updating_state_dynamics.append(updating_state_dynamics[i].reshape(50, 50))
+# Store the energy values associated to the different states into a list
+# using the synchronous dynamics rule
+energy_list_sync = []
+for element in state_history_sync:
+    energy_list_sync.append(energy(element, weights))
 
-# reduced_updating_state_dynamics_async = updating_state_dynamics_async[::1000].copy()
-# reshaped_updating_state_dynamics_async = []
-# for i in range(len(updating_state_dynamics_async)):
-    # reshaped_updating_state_dynamics_async.append(updating_state_dynamics_async[i].reshape(50, 50))
+# Store the energy values associated to the different states into a list
+# using the asynchronous dynamics rule
+energy_list_async = []
+reduced_state_history_async = state_history_async[0::1000]
+for element in reduced_state_history_async:
+    energy_list_async.append(energy(element, weights))
 
-"""Saving the video in the current directory"""
-save_video(reshaped_updating_state_dynamics, "./video_synchronous_experiment.mp4")
-# save_video(updating_state_dynamics_async, "./video_asynchronous_experiment.mp4")
+# Plotting the energy versus time
+plt.figure(figsize=(5, 7))
+plt.subplot(211)
+plt.plot(np.arange(0, len(state_history_sync), step=1), energy_list_sync, color='blue')
+plt.ylabel("Energy")
+plt.title("Plot of the energy versus the time computed with the synchronous rule")
+plt.subplot(212)
+plt.plot(np.arange(0, len(reduced_state_history_async), step=1), energy_list_async, color='red')
+plt.xlabel("Time [s]")
+plt.ylabel("Energy")
+plt.title("Plot of the energy versus the time computed with the asynchronous rule")
+plt.show()
 
-# ---------------------------------------------------------------------------------------------------------------------------------
+# Constituting a list of matrices of shape 50x50 to constitute the video
+# (for the synchronous dynamics rule)
+reshaped_state_history_sync = []
+for element in state_history_sync:
+    reshaped_state_history_sync.append(element.reshape(50, 50))
 
+# Constituting a list of matrices of shape 50x50 to constitute the video
+# (for the asynchronous dynamics rule with only storing one state every 1000)
+reduced_state_history_async = state_history_async[0::1000]
+reshaped_state_history_async = []
+for element in reduced_state_history_async:
+    reshaped_state_history_async.append(element.reshape(50, 50))
+
+# Saving the video in the current directory
+save_video(reshaped_state_history_sync, "./video_synchronous_experiment.mp4")
+save_video(reshaped_state_history_async, "./video_asynchronous_experiment.mp4")
