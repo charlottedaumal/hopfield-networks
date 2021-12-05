4339a5a83b669c1b61efb5580e2f26d2ba5880f9
ClemenceKiehl <92544192+ClemenceKiehl@users.noreply.github.com>
2021-12-05 22:57:46 +0100

    changed some pytests and added pytest for energy

--- test_HopfieldNetwork.py
+++ test_HopfieldNetwork.py
@@ -19,22 +19,39 @@ def test_perturb_pattern():
     ''' testing if perturbed pattern is different '''
     assert((perturb_pattern(np.array([[1, 1, 2, 3]]), 7) != np.array([[1, 1, 2, 3]])).any())
 
-    
+
+
 def test_update():
-    assert(update(np.array([[2, 5, 6, 7], [4, 5, 6, 9]]), np.array([[1, 1], [1, 1]])).all() == (np.array([[1, 1, 1, 1], [1, 1, 1, 1]])).all())
+
+    assert (update(np.array([[2, 5, 6, 7], [4, 5, 6, 9]]), np.array([[1, 1], [1, 1]])).all() == (
+        np.array([[1, 1, 1, 1], [1, 1, 1, 1]])).all())
 
     '''testing the values of the updated pattern'''
     list = [-1, 1]
     assert((update(np.array([[2, 5, 6, 7], [4, 5, 6, 9]]), np.array([[1, 1], [1, 1]]))).all() in list)
 
+    """testing if updated pattern is different"""
+    assert((update(np.array([[1, 1, 1, 1], [4, 5, 6, 9]]), np.array([[1, 1], [1, 1]])) != np.array([[1, 1, 1, 1], [4, 5, 6, 9]])).any())
+
 
 def test_update_async():
-    assert(update_async(np.array([[8, 9], [0, 0]]), np.array([[1, 1], [2, 2]])).all() == (np.array([[1, 1]])).all())
+
+    assert (update_async(np.array([[8, 9], [0, 0]]), np.array([[1, 1], [2, 2]])).all() == (np.array([[1, 1]])).all())
+
+    """testing the values of the updated pattern"""
+    list = [-1, 1]
+    assert((update_async(np.array([[2, 5, 6, 7], [4, 5, 6, 9]]), np.array([[1, 1], [1, 1]]))).all() in list)
+
+    """testing if updated pattern is different"""
+    assert((update_async(np.array([[1, 1, 1, 1], [4, 5, 6, 9]]), np.array([[1, 1], [1, 1]])) != np.array(
+        [[1, 1, 1, 1], [4, 5, 6, 9]])).any())
 
 
 def test_hebbian_weights():
+    weight_matrix = hebbian_weights(np.array([[1, 1, -1, -1], [1, 1, -1, 1], [-1, 1, -1, 1]]))
+
     '''testing the symmetry of the matrix'''
-    assert(np.allclose(hebbian_weights(np.array([[1, 1, -1, -1], [1, 1, -1, 1], [-1, 1, -1, 1]])), np.transpose(hebbian_weights(np.array([[1, 1, -1, -1], [1, 1, -1, 1], [-1, 1, -1, 1]])))))
+    assert(np.allclose(weight_matrix, np.transpose(weight_matrix)))
 
     '''testing the size of the matrix'''
     weight_matrix = hebbian_weights(np.array([[1, 1, -1, -1], [1, 1, -1, 1], [-1, 1, -1, 1]]))
@@ -45,10 +62,30 @@ def test_hebbian_weights():
 
 
 def test_storkey_weights():
+    weight_matrix = storkey_weights(np.array([[1, 1, -1, -1], [1, 1, -1, 1], [-1, 1, -1, 1]]))
+
     '''testing the symmetry of the matrix'''
-    assert(np.allclose(storkey_weights(np.array([[1, 1, -1, -1], [1, 1, -1, 1], [-1, 1, -1, 1]])),
-                        np.transpose(storkey_weights(np.array([[1, 1, -1, -1], [1, 1, -1, 1], [-1, 1, -1, 1]])))))
+    assert(np.allclose(weight_matrix, np.transpose(weight_matrix)))
 
     '''testing the size of the matrix'''
-    weight_matrix = storkey_weights(np.array([[1, 1, -1, -1], [1, 1, -1, 1], [-1, 1, -1, 1]]))
-    assert(weight_matrix.shape[0] == weight_matrix.shape[1])
+     assert(weight_matrix.shape[0] == weight_matrix.shape[1])
+
+
+def test_dynamics():
+    a = dynamics(np.array([[1, 4, 6, 7], [5, 8, 9, 0]]), np.array([[1, 1], [1, 1]]), 10)
+    b = [np.array([[1, 4, 6, 7]]), np.array([[5, 8, 9, 0]]), np.array([[1, 1, 1, 1]]), np.array([[1, 1, 1, 1]])]
+    assert(a == b)
+
+
+def test_dynamics_async():
+    a = dynamics_async(np.array([[1, 0, 9, 7], [3, 7, 8, 9]]), np.array([[1, 5], [4, 9]]), 10, 6)
+    b = [np.array([[1, 0, 9, 7]]), np.array([[3, 7, 8, 9]]), np.array([[1, 1, 1, 1]]), np.array([[1, 1, 1, 1]]), np.array([[1, 1, 1, 1]]), np.array([[1, 1, 1, 1]]), np.array([[1, 1, 1, 1]]), np.array([[1, 1, 1, 1]]), np.array([[1, 1, 1, 1]]), np.array([[1, 1, 1, 1]]), np.array([[1, 1, 1, 1]]), np.array([[1, 1, 1, 1]]), np.array([[1, 1, 1, 1]]), np.array([[1, 1, 1, 1]])]
+    assert(a == b)
+
+
+def test_energy():
+    a = np.array([[2, 5, 6, 7], [4, 5, 6, 9]])
+    w = np.array([[1, 1], [1, 1]])
+    assert(energy(a, w) == [(np.array([[-18.,  -50.,  -72., -128.]]), np.array([[-18.,  -50.,  -72., -128.]]), np.array([[-18.,  -50.,  -72., -128.]]), np.array([[-18.,  -50.,  -72., -128.]]).all())])
+
+    assert(energy())
