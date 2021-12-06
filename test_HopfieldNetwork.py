47e73393e45bc47400bc955d0f52cb85eafc84dc
ClemenceKiehl <92544192+ClemenceKiehl@users.noreply.github.com>
2021-12-06 10:42:31 +0100

    relecture of the code

--- test_HopfieldNetwork.py
+++ test_HopfieldNetwork.py
@@ -2,8 +2,9 @@ from functions import *
 
 
 def test_generate_patterns():
-    '''testing the values of the  generated patterns '''
     list = [-1, 1]
+    
+    '''testing the values of the  generated patterns '''
     assert((generate_patterns(5, 10)).all() in list)
 
     '''testing the size of the generated patterns'''
@@ -12,13 +13,13 @@ def test_generate_patterns():
 
 
 def test_perturb_pattern():
-    '''testing the values of the perturbed patterns'''
     list = [-1, 1]
+    
+    '''testing the values of the perturbed pattern'''
     assert((perturb_pattern(np.array([[1, 1, 2, 3]]), 7)).all() in list)
 
-    ''' testing if perturbed pattern is different '''
+    '''testing if the perturbed pattern is different from the original one'''
     assert((perturb_pattern(np.array([[1, 1, 2, 3]]), 7) != np.array([[1, 1, 2, 3]])).any())
-
 
 
 def test_update():
@@ -31,10 +32,9 @@ def test_update():
     assert(np.allclose(update(p, w), u))
 
     '''testing the values of the updated pattern'''
-
     assert((update(p, w)).all() in list)
 
-    """testing if updated pattern is different"""
+    '''testing if the updated pattern is different'''
     assert((update(q, w) != q).any())
 
 
@@ -46,11 +46,11 @@ def test_update_async():
 
     assert(np.allclose(update_async(p, w), np.array([[1, 1]])))
 
-    """testing the values of the updated pattern"""
+   '''testing the values of the updated pattern'''
     list = [-1, 1]
     assert((update_async(q, w_)).all() in list)
 
-    """testing if updated pattern is different"""
+    '''testing if the updated pattern is different'''
     assert((update_async(q, w_) != q).any())
 
 
@@ -64,7 +64,7 @@ def test_hebbian_weights():
     weight_matrix = hebbian_weights(np.array([[1, 1, -1, -1], [1, 1, -1, 1], [-1, 1, -1, 1]]))
     assert (weight_matrix.shape[0] == weight_matrix.shape[1])
 
-    '''testing the diagonal elements are equal to 0'''
+    '''testing if the diagonal elements are equal to 0'''
     assert((np.diagonal(hebbian_weights(np.array(([[1, 1, -1, -1], [1, 1, -1, 1], [-1, 1, -1, 1]]))))).all() == 0)
 
 
@@ -81,9 +81,9 @@ def test_storkey_weights():
 def test_dynamics():
     s = np.array([[1, 4, 6, 7], [5, 8, 9, 0]])
     w = np.array([[1, 1], [1, 1]])
-
     a = dynamics(s, w, 10)
     b = [np.array([[1, 4, 6, 7], [5, 8, 9, 0]]), np.array([[1, 1, 1, 1], [1, 1, 1, 1]])]
+    
     assert(np.allclose(np.array([a]), np.array([b])))
 
 
@@ -92,6 +92,7 @@ def test_dynamics_async():
     w = np.array([[1, 5], [4, 9]])
     a = dynamics_async(s, w, 10, 6)
     b = [np.array([[1, 0, 9, 7], [3, 7, 8, 9]]), np.array([[1, 1, 1, 1], [1, 1, 1, 1]]), np.array([[1, 1, 1, 1], [1, 1, 1, 1]]), np.array([[1, 1, 1, 1], [1, 1, 1, 1]]), np.array([[1, 1, 1, 1], [1, 1, 1, 1]]), np.array([[1, 1, 1, 1], [1, 1, 1, 1]]), np.array([[1, 1, 1, 1], [1, 1, 1, 1]])]
+    
     assert(np.allclose(np.array([a]), np.array([b])))
 
 
@@ -100,4 +101,5 @@ def test_energy():
     w = np.array([[1, 1], [1, 1]])
     a = energy(s, w)
     b = [np.array([[-18.,  -50.,  -72., -128.]]), np.array([[-18.,  -50.,  -72., -128.]]), np.array([[-18.,  -50.,  -72., -128.]]), np.array([[-18.,  -50.,  -72., -128.]])]
+    
     assert(np.allclose(np.array([a]), np.array([b])))
