2910718c10841ffb130843bbc759d65262633af1
charlottedaumal <92551313+charlottedaumal@users.noreply.github.com>
2021-12-13 20:01:50 +0100

    Update dynamics_cython.py

--- dynamics_cython.py
+++ dynamics_cython.py
@@ -20,7 +20,7 @@ def dynamics_async(state, weights, max_iter, convergence_num_iter):
     nb_iter = nb_iter_convergence = 0
     while (nb_iter < max_iter) and (
             nb_iter_convergence < convergence_num_iter):  # two conditions to run the dynamical system : a maximum number of iterations and a minimum number of convergence iterations
-        new_state = update(previous_state, weights)  # updating the state
+        new_state = update_async(previous_state, weights)  # updating the state
         state_history.append(new_state)  # adding the updated state to the state history list
         if np.allclose(previous_state,
                        new_state):  # verifies if the state before the update is equal to the updated state
