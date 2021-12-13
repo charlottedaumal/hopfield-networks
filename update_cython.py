dfd59a2ba6797e024943ce5967f914cfaae7cb64
ClemenceKiehl <92544192+ClemenceKiehl@users.noreply.github.com>
2021-12-13 21:44:52 +0100

    Update update_cython.py

--- update_cython.py
+++ update_cython.py
@@ -25,6 +25,24 @@ def update(state, weights):
 
 
 def update_async(state, weights):
+    """Applies the asynchronous update rule to a state pattern
+    
+    Parameters:
+    --------------
+    state : array
+    -> the network state to which we will apply the asynchronous update rule
+    weights : array
+    -> weights matrix
+    
+    Output:
+    --------------
+    returns the new state updated from the previous one (a numpy array)
+    
+    Examples:
+    --------------
+    >>> update_async(np.array([-1, -1, -1, 1]), np.array([[1, 1, -1, -1], [1, 1, 1, 1]]))
+     array([-1, -1, -1,  1])
+    """
     index = rd.choices(np.linspace(0, weights.shape[0] - 1, weights.shape[0], dtype=int))  # chooses randomly an index
     pattern = state.copy()
     pattern[index] = np.where(np.dot(weights[index], state) >= 0, 1, -1) # applying the asynchronous update rule
